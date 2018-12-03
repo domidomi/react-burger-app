f957496aacb5f957d87af7a7c874863e96c3d905
domidomi <dpodgorskad@gmail.com>
2018-12-03 15:26:50 +0100

    Burger component created

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = (props) => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="bacon" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="salad" />
+            <BurgerIngredient type="bread-bottom" />    
+        </div>
+    )
+}
+
+export default Burger
